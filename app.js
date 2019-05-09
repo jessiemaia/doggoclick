79e244ebc623764d46b87a4a5aa6436ce0914db3
Jessie <jessiempaxton@gmail.com>
2019-05-09 16:59:28 -0600

    push

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
-console.log("hooked up fine")
-let doggo = {
+let activeDoggo = 0
+
+let doggos = [{
   name: "Sophia",
   moods: ["sad", "alert", "excited", "body-slam"],
   pets: 0,
   moodIndex: 0,
   tolerance: 2,
-  images: ['/assets/saddoggo.jpg', '/assets/alertdoggo.jpg', '/assets/exciteddoggo.jpg', 'bodyslamdoggo.png']
-}
+  images: ['/assets/saddoggo.jpg', '/assets/alertdog2.jpeg', '/assets/exciteddog2.jpg', '/assets/bodyslamdoggo.png']
+}, {
+  name: "Poppy",
+  moods: ["sad", "alert", "excited", "body-slam"],
+  pets: 0,
+  moodIndex: 0,
+  tolerance: 6,
+  images: ['/assets/smallfat.jpg', '/assets/alertdog2.jpeg', '/assets/exciteddog2.jpg', '/assets/bodyslamdoggo.png']
+}, {
+  name: "Jaxon",
+  moods: ["sad", "alert", "excited", "body-slam"],
+  pets: 0,
+  moodIndex: 0,
+  tolerance: 4,
+  images: ['/assets/black-lab-300x400.jpg', '/assets/alertdog2.jpeg', '/assets/exciteddog2.jpg', '/assets/bodyslamdoggo.png']
+}]
 
 function pet() {
-  doggo.pets++
-  if (doggo.pets % doggo.tolerance == 0) {
-    doggo.moodIndex++;
+  doggos[activeDoggo].pets++
+  if (doggos[activeDoggo].pets % doggos[activeDoggo].tolerance == 0) {
+    doggos[activeDoggo].moodIndex++;
   }
   drawDoggo()
 }
 
 function drawDoggo() {
-  document.getElementById("pets").innerText = doggo.pets
-  document.getElementById("doggo-image").setAttribute("src", doggo.images[doggo.moodIndex])
-  document.getElementById("mood").innerText = doggo.moods[doggo.moodIndex]
-  if (doggo.moodIndex == doggo.moods.length - 1) {
+  document.getElementById("pets").innerText = doggos[activeDoggo].pets
+  document.getElementById("doggo-image").setAttribute("src", doggos[activeDoggo].images[doggos[activeDoggo].moodIndex])
+  document.getElementById("mood").innerText = doggos[activeDoggo].mood[doggos[activeDoggo].moodIndex]
+  if (doggos[activeDoggo].moodIndex == doggos[activeDoggo].moods.length - 1) {
     document.getElementById('pet-button').disabled = true;
-  } else {
+  }
+  else {
     document.getElementById('pet-button').disabled = false;
   }
 }
 
 function treats() {
-  doggo.moodIndex = 0
-  doggo.pets = 0
+  doggos[activeDoggo].moodIndex = 0
+  doggos[activeDoggo].pets = 0
   drawDoggo()
 }
+
+function setActiveDoggo(index) {
+  activeDoggo = index
+  drawDoggo()
+}
+
+function drawButtons() {
+  let template = ''
+  for (let i = 0; i < doggos.length; i++) {
+    let doggo = doggos[i];
+    template += `
+    <button class="btn btn-primary" onclick="setActiveDoggo(${i})">${doggo.name}</button>
+      `
+  }
+  document.getElementById('doggo-buttons').innerHTML = template
+}
+
+drawButtons()
